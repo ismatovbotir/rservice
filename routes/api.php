ad7f606b9f2bac611e7cdec38ba1bafa1ad1db82
Botir <tiboruz@gmail.com>
2025-04-28 12:47:00 +0500

    telegram post

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TelegramController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/telegram', function (Request $request) {
-    return $request->user();
-});
+Route::post('/telegram', [TelegramController::class,'store']);
 
 
 
