f08db64de04edf74cbe45faeb2abbb30900cc32f
Botir <tiboruz@gmail.com>
2025-04-28 12:15:23 +0500

    api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/telegram', function (Request $request) {
+    return $request->user();
+});
+
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
