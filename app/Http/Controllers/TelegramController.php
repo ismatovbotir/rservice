ff129f9a2d37f39a891304a217a478dba2d66614
Botir <tiboruz@gmail.com>
2025-05-12 13:28:03 +0500

    fsdfafarferfae

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -9,43 +9,37 @@ use Illuminate\Support\Facades\Http;
 
 class TelegramController extends Controller
 {
-    
-    public $text='';
+
+    public $text = '';
     public $tUser;
-    public $error='';
-    public $keyboard='';
-    
+    public $error = '';
+    public $keyboard = '';
 
-    private function sendMessage($id = ''){
-        if($id==''){
-            $id=env('CHAT_ID');
+
+    private function sendMessage($id = '')
+    {
+        if ($id == '') {
+            $id = env('CHAT_ID');
         }
-        if ($this->keyboard==''){
-           $response = Http::withBody(json_encode(
-            [
-                "chat_id" => $id,
-                "text" => $this->text,
-                "parse_mode" => "HTML"
-            ]
-        ))
-            ->post("https://api.telegram.org/bot".env('TELEGRAM')."/sendMessage");
-
- 
-        }else{
+        if ($this->keyboard == '') {
             $response = Http::withBody(json_encode(
                 [
                     "chat_id" => $id,
                     "text" => $this->text,
-                    'reply_markup'=>$this->keyboard
+                    "parse_mode" => "HTML"
                 ]
             ))
-                ->post("https://api.telegram.org/bot".env('TELEGRAM')."/sendMessage");
-    
-     
-
+                ->post("https://api.telegram.org/bot" . env('TELEGRAM') . "/sendMessage");
+        } else {
+            $response = Http::withBody(json_encode(
+                [
+                    "chat_id" => $id,
+                    "text" => $this->text,
+                    'reply_markup' => $this->keyboard
+                ]
+            ))
+                ->post("https://api.telegram.org/bot" . env('TELEGRAM') . "/sendMessage");
         }
-        
-
     }
     public function index()
     {
@@ -65,60 +59,55 @@ class TelegramController extends Controller
      */
     public function store(Request $request)
     {
-        
+
 
         //if (in_array( $ipAddress,$telegramIpRanges)) {
-            
-            $data = $request->all();
-           // $this->text=json_encode($data);
-            //$this->sendMessage();
-            if (!isset($data['update_id']) ) {
-                $this->text="someone is hacking system";
-                $this->sendMessage();
-                return response()->json([
-                    'status' => 'ok'
-                ], 200);
 
-                // Нет update_id — неправильный запрос
-                //return response()->json(['error' => 'incorrect data was sent'], 400);
-            }
+        $data = $request->all();
+        // $this->text=json_encode($data);
+        //$this->sendMessage();
+        if (!isset($data['update_id'])) {
+            $this->text = "someone is hacking system";
+            $this->sendMessage();
+            return response()->json([
+                'status' => 'ok'
+            ], 200);
 
-            if (isset($data['message'])){
+            // Нет update_id — неправильный запрос
+            //return response()->json(['error' => 'incorrect data was sent'], 400);
+        }
 
-                $telegramUser=$this->user($data['message']['from']);
+        if (isset($data['message'])) {
 
-                if($telegramUser==null){
+            $telegramUser = $this->user($data['message']['from']);
 
-                    $this->text="Could not find and create user";
-                    $this->sendMessage($data['message']['from']['id']);
+            if ($telegramUser == null) {
 
-                   
-                }else{
-                    if($telegramUser->phone==null){
-                        if(isset($data['message']['contact'])){
-                            if($data['message']['contact']['user_id']==$telegramUser->id){
-                                Telegram::where('id',$telegramUser->id)->update([
-                                    'phone'=>$data['message']['contact']['phone_number']
-                                ]);
-                                $this->text="Raqamingiz royixatga kiritildi, murojaat kiritish uchun iltimos email yuboring";
-                                $this->keyboard=json_encode([
-                                    'remove_keyboard' => true
-                                ]);
-                            }else{
-                                $this->text="Bu sizning raqam emas(yoki telegramdan boshqa raqam ishlatasiz).Iltimos qaytada nyuboring";
-                            }
-                            $this->sendMessage();
-
-                           
-                            return response()->json([
-                                'status' => 'ok'
-                            ], 200);
-
+                $this->text = "Could not find and create user";
+                $this->sendMessage($data['message']['from']['id']);
+            } else {
+                if ($telegramUser->phone == null) {
+                    if (isset($data['message']['contact'])) {
+                        if ($data['message']['contact']['user_id'] == $telegramUser->id) {
+                            Telegram::where('id', $telegramUser->id)->update([
+                                'phone' => $data['message']['contact']['phone_number']
+                            ]);
+                            $this->text = "Raqamingiz royixatga kiritildi, murojaat kiritish uchun iltimos email yuboring";
+                            $this->keyboard = json_encode([
+                                'remove_keyboard' => true
+                            ]);
+                        } else {
+                            $this->text = "Bu sizning raqam emas(yoki telegramdan boshqa raqam ishlatasiz).Iltimos qaytadan yuboring";
                         }
-                        
-                        
-                        $this->text="Royxatdan otish uchun telefon raqamingizni yuboring";
-                        $this->keyboard= json_encode([
+                        $this->sendMessage();
+
+
+                        return response()->json([
+                            'status' => 'ok'
+                        ], 200);
+                    } else {
+                        $this->text = "Royxatdan otish uchun telefon raqamingizni yuboring";
+                        $this->keyboard = json_encode([
                             'keyboard' => [
                                 [
                                     [
@@ -130,58 +119,57 @@ class TelegramController extends Controller
                             'resize_keyboard' => true,
                             'one_time_keyboard' => true
                         ]);
-
-
-
-                        $this->sendMessage($telegramUser->id);
-                        
                     }
 
-                    $text=$data['message']['text'];
-                    
-                    return response()->json([
-                        'status' => 'ok'
-                    ], 200);
-                   
-                    
 
-                    
-                    
-                    //$this->text=json_encode($telegramUser);
-                    //$this->sendMessage();
-                }
-                
-            }else if(isset($data['edited_message'])){
-                $user=$this->user($data['edited_message']['from']);
 
-                if($user==null){
-                    return response()->json(['error' => $this->text], 401);
-                }else{
 
-                    return response()->json(['client' => $user]);
+
+
+                    $this->sendMessage($telegramUser->id);
                 }
 
+                $text = $data['message']['text'];
+
+                return response()->json([
+                    'status' => 'ok'
+                ], 200);
 
 
-            }else{
-                return response()->json(['error' => 'incorrect data was sent'], 400);
+
+
+
+                //$this->text=json_encode($telegramUser);
+                //$this->sendMessage();
             }
-            
+        } else if (isset($data['edited_message'])) {
+            $user = $this->user($data['edited_message']['from']['id']);
+
+            if ($user == null) {
+                return response()->json(['error' => $this->text], 401);
+            } else {
+
+                return response()->json(['client' => $user]);
+            }
+        } else {
+            return response()->json(['error' => 'incorrect data was sent'], 400);
+        }
 
 
 
 
 
-            $this->text= $data;
-            //$this->sendMessage();
-            
 
-            return response()->json([
-                'status' => 'ok'
-            ], 200);
+        $this->text = $data;
+        //$this->sendMessage();
+
+
+        return response()->json([
+            'status' => 'ok'
+        ], 200);
         //} else {
-          //  return 0;
-      // }
+        //  return 0;
+        // }
     }
 
     /**
@@ -216,23 +204,23 @@ class TelegramController extends Controller
         //
     }
 
-    private function user($from){
-        $id = $from['id'];
-        $telegram = Telegram::find($id);
+    private function user($data)
+    {
+
+        $telegram = Telegram::find($data['id']);
         //$this->text = $telegram;
         //$this->sendMessage();
 
         if ($telegram == null) {
-            
-           
 
-            try{
+
+
+            try {
                 $telegram = Telegram::create(
-                $from
-            ); 
-            }catch(QueryException $e){
-                $this->text=$e->getMessage();
-                
+                    $data
+                );
+            } catch (QueryException $e) {
+                $this->text = $e->getMessage();
             }
         }
         return $telegram;
